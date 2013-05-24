5270942bf0d2390300572bafd48fdd7f2a12b60e
Oscar Casas <user@falso.com>
2013-05-24 10:19:25 -0500

    Mejora en comentarios

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
-Este es una librería de C para calcular CRC8
+Este es una libreria de C para calcular CRC8
 
-La constante GP contiene el polinomio característico
+La constante GP contiene el polinomio caracteristico
 La constante ID contiene contiene el resultado de calcular CRC8 para 0x01
 
 
--- crc8.c
+++ crc8.c
@@ -2,9 +2,12 @@
 #define DI  0xAB      //primer dato
 
 
-static unsigned char crc8_table[256];     /* 8-bit table */
-static int made_table=0;
+static unsigned char crc8_table[256];   // tabla de 8-bit
+static int made_table=0;                // Flag de realizacion de tabla
 
+
+//------------------------------------------------------------
+//Funcion de inicializacion de tabla de 8-bit
 static void init_crc8()
 {
   int i,j;
@@ -23,6 +26,8 @@ static void init_crc8()
 }
 
 
+//------------------------------------------------------------
+//Calculo de CRC8 a m. Se añade en *crc
 void crc8(unsigned char *crc, unsigned char m)
 {
   if (!made_table)
